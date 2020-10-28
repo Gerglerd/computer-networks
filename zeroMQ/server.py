13ddefe103c2e8b9031dd24d18124efb16dcc271
Gerglerd <54989664+Gerglerd@users.noreply.github.com>
2020-10-28 13:55:51 +0700

    Update server.py

--- zeroMQ/server.py
+++ zeroMQ/server.py
@@ -3,13 +3,13 @@ import zmq
 
 context = zmq.Context()
 socket = context.socket(zmq.REP)
-socket.bind("tcp://*:5555")
+socket.bind("tcp://127.0.0.1:5000")
 
 while True:
     # wait next request
     message = socket.recv()
-    print("Received request: %s" % message)
+    print("Received request 1: %s" % message)
     # working
-    time.sleep(0.5)
+    time.sleep(1)
     # send reply back
     socket.send(message)
