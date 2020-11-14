a07226e95c2eea1686d7d75e438aa4f5e4fdeec1
Gerglerd <54989664+Gerglerd@users.noreply.github.com>
2020-11-14 23:08:52 +0700

    Update client.py

--- simpleProxy/client.py
+++ simpleProxy/client.py
@@ -1,11 +1,9 @@
 import socket
 
 while True:
-    # message = input()
-    for i in range(500):
-        message = i
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock.connect(('localhost', 9091))
-        sock.send(message.encode())
-        message = sock.recv(1024)
-        print('message from server: ' + message.decode())
+    message = input()
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect(('localhost', 9091))
+    sock.send(message.encode())
+    message = sock.recv(1024)
+    print('message from server: ' + message.decode())
