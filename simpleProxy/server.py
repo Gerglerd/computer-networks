8b45781c1af3b2e19da7252f4af4806276f45bc5
Gerglerd <54989664+Gerglerd@users.noreply.github.com>
2020-11-14 22:30:15 +0700

    Update server.py

--- simpleProxy/server.py
+++ simpleProxy/server.py
@@ -1,17 +1,15 @@
 import socket
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.bind(('localhost', 5556))
-sock.listen(10)
-server_message = 'Hello'
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # создание нового сокета и возвращение fd
+sock.bind(('localhost', 5556))  # связать сокет с IP-адресом и портом
+sock.listen(10)  # слушать порт и ожидать когда будет установлено соеденение
+server_message = 'Hello from server!\n'
 
 while True:
-    server_sock, server_addr = sock.accept()
-    data = server_sock.recv(1024)
+    conn, addr = sock.accept()  # запрос на установление соеденения с клиентом
+    data = conn.recv(1024)  # получить данные из сети
     if not data:
         break
     print('received message: ' + data.decode())
     data = data + server_message.encode()
-    server_sock.send(data)
-
-server_sock.close()
+    conn.send(data)  # отправить данные по сети
