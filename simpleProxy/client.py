3e0e332676e726e0b29c68b55a9bf9d63d944d1a
Gerglerd <54989664+Gerglerd@users.noreply.github.com>
2020-11-18 16:03:09 +0700

    Delete client.py

--- simpleProxy/client.py
+++
@@ -1,9 +0,0 @@
-import socket
-
-while True:
-    message = input()
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.connect(('localhost', 9091))
-    sock.send(message.encode())
-    message = sock.recv(1024)
-    print('message from server: ' + message.decode())
