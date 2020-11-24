1e8c37027afa73e506216900df32d9bf96e1e436
gerglerd <54989664+Gerglerd@users.noreply.github.com>
2020-11-25 01:46:25 +0700

    Delete client.py

--- http/client.py
+++
@@ -1,8 +0,0 @@
-import socket
-
-client_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_sock.connect(('0.0.0.0', 53210))
-client_sock.sendall(b'Hello World!')
-data = client_sock.recv(1024)
-client_sock.close()
-print('Received', repr(data))
