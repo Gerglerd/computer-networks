b062a4e9997ea7fe076e81168ed2c25093f18336
Sayfudinova Polina <54989664+gergler@users.noreply.github.com>
2020-11-25 17:55:26 +0700

    Update server.py

--- simple_proxy/server.py
+++ simple_proxy/server.py
@@ -1,15 +1,18 @@
 import socket
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # создание нового сокета и возвращение fd
-sock.bind(('localhost', 5556))  # связать сокет с IP-адресом и портом
+sock.bind(('localhost', 5555))  # связать сокет с IP-адресом и портом
 sock.listen(10)  # слушать порт и ожидать когда будет установлено соеденение
 server_message = '(this message from server)\n'
+conn, addr = sock.accept()  # запрос на установление соеденения с клиентом
 
 while True:
-    conn, addr = sock.accept()  # запрос на установление соеденения с клиентом
     data = conn.recv(1024)  # получить данные из сети
     if not data:
         break
     print('received message: ' + data.decode())
-    # data = data + server_message.encode()
+    data = data + server_message.encode()
     conn.send(data)  # отправить данные по сети
+
+print('Server closed')
+conn.close()
