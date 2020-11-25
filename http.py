53ba46607201dc950146cfe27bb96aaa63bbdbb1
Sayfudinova Polina <54989664+gergler@users.noreply.github.com>
2020-11-25 16:09:00 +0700

    Update http.py

--- http.py
+++ http.py
@@ -5,18 +5,18 @@ import http.server
 import socketserver
 import sys
 
-file_path = sys.argv[1]
-file_path.split('/')
-length = len(file_path)
-for i in range(length - 1):
-    path = '/' + file_path[i]
+# file_path = sys.argv[1]
+# file_path.split('/')
+# length = len(file_path)
+# for i in range(length - 1):
+#     path = '/' + file_path[i]
 
 
 class MyHttpRequestHandler(http.server.SimpleHTTPRequestHandler):
 
     def do_GET(self):
-        if self.path == path:
-            self.path = file_path[length]
+        if self.path == '/':
+            self.path = 'mywebhtml.html'
         return http.server.SimpleHTTPRequestHandler.do_GET(self)
 
 
