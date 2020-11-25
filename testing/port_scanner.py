9a5e73ca53fb1259fd09761a91edb9b1165a014a
Sayfudinova Polina <54989664+gergler@users.noreply.github.com>
2020-11-25 18:14:32 +0700

    Update port_scanner.py

--- testing/port_scanner.py
+++ testing/port_scanner.py
@@ -5,9 +5,8 @@ startTime = time.time()
 
 if __name__ == '__main__':
     target = input('Enter the host to be scanned: ')
-    target_IP = gethostbyname(
-        target)  # given a host name the gethostbyname() function returns the IP address of the host
-    print('Sterting scan on host: ', target_IP)
+    target_IP = gethostbyname(target)  # given a host name the gethostbyname() function returns the IP address of the host
+    print('Starting scan on host: ', target_IP)
 
     for i in range(50, 500):
         s = socket(AF_INET, SOCK_STREAM)
