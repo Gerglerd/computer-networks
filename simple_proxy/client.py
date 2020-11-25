9e6f6c56f3568aeb0e131afd6a34dd5d3de3a77a
Sayfudinova Polina <54989664+gergler@users.noreply.github.com>
2020-11-25 17:55:06 +0700

    Update client.py

--- simple_proxy/client.py
+++ simple_proxy/client.py
@@ -1,9 +1,9 @@
 import socket
 
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.connect(('localhost', 9091))
 while True:
     message = input()
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.connect(('localhost', 9091))
     sock.send(message.encode())
     message = sock.recv(1024)
     print('message from server: ' + message.decode())
